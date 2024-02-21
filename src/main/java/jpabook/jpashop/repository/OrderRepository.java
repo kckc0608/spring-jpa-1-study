1726e9637bda3a5b54a200260f7fc6f3ab7b1dea
kckc0608 <kckc0608@gmail.com>
2024-02-22 00:59:19 +0900

    fix: order repository 오타 수정

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderRepository {
     private final EntityManager em;
-    public void save(Order oder) {
+    public void save(Order order) {
         em.persist(order);
     }
 
